e813ea316913268e6dbc339996be02bbd8cee2e3
anjali411 <chourdiaanjali123@gmail.com>
2021-12-20 22:01:13 +0000

    Per-overload torch.ops API
    ghstack-source-id: b254ef2411791f53c153380214114f1771a11a9d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/67254

--- torch/_ops.py
+++ torch/_ops.py
@@ -25,6 +25,98 @@ def dl_open_guard():
     if _SET_GLOBAL_FLAGS:
         sys.setdlopenflags(old_flags)
 
+class OpOverload:
+    def __init__(self, overloadpacket, op, schema):
+        self.op = op
+        self.schema = schema
+        self.overloadpacket = overloadpacket
+
+    # it's a no-op since OpOverload object is immutable and must be for a given op overload.
+    def __deepcopy__(self, memo=None):
+        return self
+
+    def __str__(self):
+        return "OpOverload(op='{}.{}', overload='{}')".format(*self.qualified_name().split("::"), self.overload_name())
+
+    def __call__(self, *args, **kwargs):
+        return self.op(*args, **kwargs or {})
+
+    def __getattr__(self, key):
+        return getattr(self.op, key)
+
+    # `my_namespace::my_op`
+    def qualified_name(self):
+        return self.schema.name
+
+    def overload_name(self):
+        return self.schema.overload_name
+
+    # vector of Arguments -- each Argument's name, and type can currently be accessed
+    # it might be useful to make the mutability of the arg accessible (using the alias_info stored in the C++ struct)
+    def returns(self):
+        return self.schema.returns
+
+    def arguments(self):
+        return self.schema.arguments
+
+class OpOverloadPacket():
+    def __init__(self, qualified_op_name, op_name, op):
+        self.qualified_op_name = qualified_op_name
+        self.op_name = op_name
+        self.op = op
+
+    # it's a no-op since OpOverloadPacket object is immutable and must be for a given op.
+    def __deepcopy__(self, memo=None):
+        return self
+
+    def __str__(self):
+        return "OpOverloadPacket(op='{}.{}')".format(*self.qualified_op_name.split("::"))
+
+    def __getattr__(self, key):
+        # It is not a valid op_name when __file__ is passed in
+        if key == '__file__':
+            return 'torch.ops'
+
+        throw_error = False
+
+        try:
+            use_key = "" if key == 'default' else key
+            op_ = torch._C._get_operation_overload(self.qualified_op_name, use_key)
+            schema = torch._C._get_schema(self.qualified_op_name, use_key)
+            overload = OpOverload(self, op_, schema)
+            # cache the overload object
+            setattr(self, key, overload)
+            return overload
+        except RuntimeError:
+            try:
+                # This is added to maintain bc in case the user queries an attribute that exists on `self.op`
+                # which used to be returned before instead of the OpOverloadPacket
+                out = getattr(self.op, key)
+                return out
+            except AttributeError:
+                throw_error = True
+
+        # We throw an error here as opposed to above since the stack trace and error message
+        # printed in that case is a bit confusing:
+        # RuntimeError: Found no matching schema
+        # During handling of the above exception, another exception occurred:
+        # AttributeError ...
+        if throw_error:
+            raise AttributeError("'{}' object has no attribute '{}'".format(str(self), key))
+
+    def __call__(self, *args, **kwargs):
+        # overloading __call__ to ensure torch.ops.foo.bar() is still callable from JIT
+        # We save the function ptr as the `op` attribute on OpOverloadPacket to access it here.
+        return self.op(*args, **kwargs or {})
+
+# Resolution of torch.fn is different from torch.ops. ... fn
+# First one is parsing the python objects and then matching the schema -> calls into the unboxed version of the method
+
+# second one is done by JIT. Creates a stack of all the overloads and then tries to match the correct one at runtime
+# and always calls into boxed version of the method
+
+# autograd codegen mostly creates variabletype, tracertype, inplace or view type and python bindings
+# aten codegen generates tensor methods for the the tensor class
 
 # _OpNamespace is a subclass of ModuleType because the torch script
 # allows attribute lookups on modules only. Since we want torch.ops.foo.bar()
@@ -59,14 +151,20 @@ class _OpNamespace(types.ModuleType):
             return 'torch.ops'
         # Get the op `my_namespace::my_op` if available. This will also check
         # for overloads and raise an exception if there are more than one.
-        qualified_op_name = '{}::{}'.format(self.name, op_name)
+        namespace_name = self.name
+        qualified_op_name = '{}::{}'.format(namespace_name, op_name)
         op = torch._C._jit_get_operation(qualified_op_name)
+
         # let the script frontend know that op is identical to the builtin op
         # with qualified_op_name
         torch.jit._builtins._register_builtin(op, qualified_op_name)
-        setattr(self, op_name, op)
-        op.__module__ = self.__module__ + "." + self.name
-        return op
+        op.__module__ = self.__module__ + "." + namespace_name
+        opoverloadpacket = OpOverloadPacket(qualified_op_name, op_name, op)
+        opoverloadpacket.__module__ = self.__module__ + "." + namespace_name
+        # cache the opoverloadpacket to ensure that each op corresponds to
+        # a unique OpOverloadPacket object
+        setattr(self, op_name, opoverloadpacket)
+        return opoverloadpacket
 
 class _Ops(types.ModuleType):
     __file__ = '_ops.py'
